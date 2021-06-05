384670b8f7cc9bfe05079c0c8d64bfd7984e4b66
vasalikhitha <likhithavasa1077@gmail.com>
2021-06-05 22:44:16 +0530

    second

---
+++ src/main/java/com/example/demo/BasicSpringBootProject1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBootProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBootProject1Application.class, args);
+		System.out.println("Hello Dev-Op");
+	}
+
+}
